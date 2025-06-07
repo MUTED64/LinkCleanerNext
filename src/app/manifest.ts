78859919f44fe00983c778e0e001d7fcc072d496
MUTED64 <muted32555664@gmail.com>
2025-06-08 04:32:54 +0800

    Fix PWA

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,12 +14,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/LinkCleanerNext/web-app-menifest-192x192.png',
+        src: '/LinkCleanerNext/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/LinkCleanerNext/web-app-menifest-512x512.png',
+        src: '/LinkCleanerNext/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
